ea03044dc22bb80b0a7d73c115ef33292bf88387
nishad-shamsudeen <nishadshmsudeen@gmail.com>
2025-02-01 22:55:25 +0530

    Running message alignment changed

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import react from 'react';
 import './App.css';
 import NavBar from './Components/NavBar/NavBar';
 import Banner from './Components/Banner/Banner';
-import Message from './Components/Message/Message';
+
 import {originalMovieUrl,actionMovieUrl} from './Constatnce/Constance'
 import MovieList from './Components/MovieList/MovieList';
 
@@ -14,10 +14,10 @@ function App() {
     <div>
       
       <NavBar/>
-      <Message/>
-     <Banner>
+      
+     <Banner/>
      
-     </Banner>
+     
      
      <MovieList url= {originalMovieUrl} title='Netflix Originals'/>
      <MovieList url={actionMovieUrl}title='Action' isSmall/>
--- src/Components/Message/Message.js
+++ src/Components/Message/Message.js
@@ -1,18 +1,18 @@
 import React from 'react'
 import './Message.css'
-import { Typewriter,Cursor } from 'react-simple-typewriter';
+import { Typewriter, Cursor } from 'react-simple-typewriter';
 function Message() {
-  const text=Typewriter({
-    words:['More features on the way! Thanks for your support. '],
-    loop:{},
+  const text = Typewriter({
+    words: ['More features on the way! Thanks for your support....... '],
+    loop: {},
   })
   return (
     <div>
       <h1 className='moving-message'>
-        <span style={{fontWeight:'bold',color:'white'}}>
-          {text}
-        </span>
-        <Cursor/>
+
+        {text}
+
+        <Cursor />
       </h1>
     </div>
   )
