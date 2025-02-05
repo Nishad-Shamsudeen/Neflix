3197bd0f96dfff3a8106add5919523e62999b3d3
nishad-shamsudeen <nishadshmsudeen@gmail.com>
2025-02-05 21:47:26 +0530

    message alignment changed

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import './App.css';
 import NavBar from './Components/NavBar/NavBar';
 import Banner from './Components/Banner/Banner';
+import Message from './Components/Message/Message';
 //import 'bootstrap/dist/css/bootstrap.min.css';
 import {originalMovieUrl,actionMovieUrl} from './Constatnce/Constance'
 import MovieList from './Components/MovieList/MovieList';
@@ -15,8 +16,9 @@ function App() {
       
       <NavBar/>
       
+      
      <Banner/>
-     
+     <Message/>
      
      
      <MovieList url= {originalMovieUrl} title='Netflix Originals'/>
